d837c2f91bca258ba3cdf3a2699ae93181a7a196
Jan Vigar <jansvigar@gmail.com>
2018-04-01 01:44:30 -0700

    feat: add functionality to flip card

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,10 @@
-import '../css/style.css';
+import "../css/style.css";
+
+const board = document.querySelector(".board");
+board.addEventListener("click", flipCard);
+
+function flipCard(event) {
+    if(event.target.nodeName === 'DIV') {
+        event.target.parentElement.classList.add("flipped");
+    }
+}
