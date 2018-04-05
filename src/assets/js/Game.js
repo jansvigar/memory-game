e814f3c19a7d085d4c42183a9a910bc4fc960e07
Jan Vigar <jansvigar@gmail.com>
2018-04-04 19:32:22 -0700

    feat: add feature to load cards element to board

--- src/assets/js/Game.js
+++ src/assets/js/Game.js
@@ -1,22 +1,24 @@
+import Animation from './animation';
+
 class Game {
   constructor() {
     this.cards = [
-      { name: "a", imgURL: "", matched: false },
-      { name: "b", imgURL: "", matched: false },
-      { name: "c", imgURL: "", matched: false },
-      { name: "d", imgURL: "", matched: false },
-      { name: "e", imgURL: "", matched: false },
-      { name: "f", imgURL: "", matched: false },
-      { name: "g", imgURL: "", matched: false },
-      { name: "h", imgURL: "", matched: false },
-      { name: "a", imgURL: "", matched: false },
-      { name: "b", imgURL: "", matched: false },
-      { name: "c", imgURL: "", matched: false },
-      { name: "d", imgURL: "", matched: false },
-      { name: "e", imgURL: "", matched: false },
-      { name: "f", imgURL: "", matched: false },
-      { name: "g", imgURL: "", matched: false },
-      { name: "h", imgURL: "", matched: false }
+      { name: "a", imgURL: "" },
+      { name: "b", imgURL: "" },
+      { name: "c", imgURL: "" },
+      { name: "d", imgURL: "" },
+      { name: "e", imgURL: "" },
+      { name: "f", imgURL: "" },
+      { name: "g", imgURL: "" },
+      { name: "h", imgURL: "" },
+      { name: "a", imgURL: "" },
+      { name: "b", imgURL: "" },
+      { name: "c", imgURL: "" },
+      { name: "d", imgURL: "" },
+      { name: "e", imgURL: "" },
+      { name: "f", imgURL: "" },
+      { name: "g", imgURL: "" },
+      { name: "h", imgURL: "" }
     ];
     this.flippedCards = [];
   }
@@ -24,12 +26,12 @@ class Game {
   /** Fisher-Yates Shuffling Algorithm */
   /** https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/2450976#2450976 */
   shuffleCards() {
-    let currentIndex = this.cards.length,
+    let currentIndex = this.cards.length - 1,
       temp,
       randomIndex;
 
     /** While there are still unrandomized cards */
-    while (currentIndex > 0) {
+    while (currentIndex >= 0) {
       /** Select random card from the remaining cards */
       randomIndex = Math.floor(Math.random() * currentIndex);
 
@@ -48,8 +50,11 @@ class Game {
   /** Method to start the game */
   start() {
     const boardElement = document.querySelector(".board");
-    boardElement.addEventListener("click", (event, cards, flippedCards) =>
-      _handleClickedCard(event, cards, flippedCards)
+    
+    _loadCardsToBoard(boardElement, this.cards);
+
+    boardElement.addEventListener("click", (event) =>
+      _handleClickedCard(event, this.cards, this.flippedCards)
     );
   }
 }
@@ -58,7 +63,7 @@ export default Game;
 
 function _handleClickedCard(event, cards, flippedCards) {
   const clickedElement = event.target,
-    cardIndex = clickedElement.dataset.index,
+    cardIndex = clickedElement.parentElement.dataset.index,
     clickedCard = cards[cardIndex];
 
   Animation.flip(clickedElement);
@@ -69,4 +74,36 @@ function _handleClickedCard(event, cards, flippedCards) {
 function _checkMatch(card1, card2) {
   if (card1.name === card2.name) _handleCardMatch();
   else _handleCardNotMatch();
+}
+
+function _handleCardMatch() {}
+
+function _handleCardNotMatch() {}
+
+function _loadCardsToBoard(board, cards) {
+  if (!cards.length) {
+    throw new Error("There are no card to load");
+  }
+  const fragment = document.createDocumentFragment();
+
+  cards.forEach((card, idx) => {
+    const listElement = document.createElement("li"),
+      frontDivElement = document.createElement("div"),
+      backDivElement = document.createElement("div");
+
+    listElement.classList.add("card");
+    listElement.setAttribute("data-index", idx);
+
+    frontDivElement.classList.add("front");
+    frontDivElement.textContent = card.name;
+    listElement.appendChild(frontDivElement);
+
+    backDivElement.classList.add("back");
+    backDivElement.textContent = "Back";
+    listElement.appendChild(backDivElement);
+
+    fragment.appendChild(listElement);
+  });
+
+  board.appendChild(fragment);
 }
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,7 @@
 import Animation from "./animation";
 import Game from "./Game";
 import "../css/style.css";
+
+const game = new Game();
+game.shuffleCards();
+game.start();
