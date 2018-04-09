e2d25bb116c54829fdddb6e3ea64853ec1e3d8c4
Jan Vigar <jansvigar@gmail.com>
2018-04-08 21:05:06 -0700

    feat: update image paths

--- src/assets/js/Game.js
+++ src/assets/js/Game.js
@@ -1,12 +1,12 @@
 import Animation from "./animation";
-import dogPic1 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-1.jpg";
-import dogPic2 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-2.jpg";
-import dogPic3 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-3.jpg";
-import dogPic4 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-4.jpg";
-import dogPic5 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-5.jpg";
-import dogPic6 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-6.jpg";
-import dogPic7 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-7.jpg";
-import dogPic8 from "/Users/jvigar/Documents/WebProjects/memory-game/src/assets/img/dog-pic-8.jpg";
+import dogPic1 from "../img/dog-pic-1.jpg";
+import dogPic2 from "../img/dog-pic-2.jpg";
+import dogPic3 from "../img/dog-pic-3.jpg";
+import dogPic4 from "../img/dog-pic-4.jpg";
+import dogPic5 from "../img/dog-pic-5.jpg";
+import dogPic6 from "../img/dog-pic-6.jpg";
+import dogPic7 from "../img/dog-pic-7.jpg";
+import dogPic8 from "../img/dog-pic-8.jpg";
 
 export default class Game {
   constructor() {
